a91a0335a8ea7652a20126df6aa4af36db49810a
Rathu-99 <rathurshykan99@gmail.com>
2023-12-04 23:41:07 +0530

    11.40 pm commit

--- models/User.js
+++ models/User.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true},
+    fullName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    phonenumber: {type: String, required: true},
-    address: { type: String, required: true},
+    phoneNumber: { type: String, required: true },
+    address: { type: String, required: true },
   },
   { timestamps: true }
 );
